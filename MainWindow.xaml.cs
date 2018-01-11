b4769fb4901c5620258dab006be2012f878c6b32
DJ Williamson <DerekWilliamson@gamestop.com>
2018-01-11 14:51:54 -0600

    v1.0.0 - Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using MahApps.Metro;
+using System.Collections.Generic;
+
+namespace PUBGBetterMap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public static MainWindow Current;
+        public static List<mrousavy.HotKey> Hotkeys;
+
+        public MainWindow()
+        {
+            /* 
+             * Supported accents:
+             *      Red, Green, Blue, Purple, Orange, Lime, Emerald, Teal, 
+             *      Cyan, Cobalt, Indigo, Violet, Pink, Magenta, Crimson, 
+             *      Amber, Yellow, Brown, Olive, Steel, Mauve, Taupe, Sienna
+             *      
+             * Supported themes:
+             *      BaseLight, BaseDark
+             */
+            SetTheme("Cyan", "BaseDark");
+
+            InitializeComponent();
+            this.AllowsTransparency = true;
+            this.Opacity = 0.9;
+
+            MainWindow.Current = this;
+            OverlayWindow.Current = new OverlayWindow();
+            this.slider_Opacity.Value = 80;
+            OverlayWindow.Current.Map.Opacity = this.slider_Opacity.Value / 100;
+            Hotkeys = new List<mrousavy.HotKey>();
+        }
+
+        #region Theme Mangament
+        public Accent CurrentAccent
+        {
+            get
+            {
+                return ThemeManager.DetectAppStyle(Application.Current).Item2;
+            }
+        }
+
+        public AppTheme CurrentTheme
+        {
+            get
+            {
+                return ThemeManager.DetectAppStyle(Application.Current).Item1;
+            }
+        }
+
+        public void SetTheme(string accent, string theme = "BaseLight")
+        {
+            ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent(accent), ThemeManager.GetAppTheme(theme));
+
+            try
+            {
+                this.GlowBrush = typeof(Brushes).GetProperties()
+                    .FirstOrDefault(x => x.Name == CurrentAccent.Name)
+                    .GetValue(null) as Brush;
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Uh oh. The glow brush could not be correlated to the accent of the application. Error: " + Environment.NewLine + ex.Message);
+                this.GlowBrush = null;
+            }
+        }
+        #endregion
+
+        #region Events
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Hotkeys.Add(new mrousavy.HotKey(ModifierKeys.None, Key.OemTilde, this, delegate 
+            {
+                if (OverlayWindow.Current.IsVisible) OverlayWindow.Current.Hide(); else OverlayWindow.Current.Show();
+            }));
+        }
+
+        private void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            foreach (var hotkey in Hotkeys)
+                hotkey.Dispose();
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void slider_Opacity_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            OverlayWindow.Current.Map.Opacity = e.NewValue / 100;
+        }
+        #endregion
+    }
+}
---
+++ OverlayWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PUBGBetterMap
+{
+    /// <summary>
+    /// Interaction logic for OverlayWindow.xaml
+    /// </summary>
+    public partial class OverlayWindow : Window
+    {
+        public static OverlayWindow Current;
+        public Point DefaultSize;
+        public double DefaultLeft;
+        public bool IsActivated;
+
+        public OverlayWindow()
+        {
+            InitializeComponent();
+
+            this.WindowStyle = WindowStyle.None;
+            this.AllowsTransparency = true;
+            this.ResizeMode = ResizeMode.NoResize;
+            this.Top = 0;
+            this.Left = 0;
+            this.Width = 0;
+            this.Height = 0;
+            if (SystemParameters.PrimaryScreenWidth == 1920 && SystemParameters.PrimaryScreenHeight == 1080)
+            {
+                this.Left = this.DefaultLeft = 412;
+                DefaultSize = new Point(1096, 1080);
+                this.Width = 1096;
+                this.Height = 1080;
+            }
+            else if (SystemParameters.PrimaryScreenWidth == 2560 && SystemParameters.PrimaryScreenHeight == 1080)
+            {
+                this.Left = this.DefaultLeft = 732;
+                DefaultSize = new Point(1096, 1080);
+                this.Width = 1096;
+                this.Height = 1080;
+            }
+            else
+                MessageBox.Show("Your current resolution is not yet supported.");
+
+            OverlayWindow.Current = this;
+        }
+    }
+}
